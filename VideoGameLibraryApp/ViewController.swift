37534b48906289b80db6bec83bf1d7cd46920b5a
Luana Duarte <luludsf@gmail.com>
2026-05-30 14:06:41 -0300

    adds initial viewcode project setup

---
+++ VideoGameLibraryApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoGameLibraryApp
+//
+//  Created by Luana Duarte on 30/05/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+
+
+}
+
