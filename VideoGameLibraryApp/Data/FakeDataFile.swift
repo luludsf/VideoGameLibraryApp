4c84dbeeda6b3b159631e234729c88138ffd91b8
Luana Duarte <luludsf@gmail.com>
2026-05-30 14:54:56 -0300

    creates project folder organization

---
+++ VideoGameLibraryApp/Data/FakeDataFile.swift
@@ -0,0 +1,8 @@
+//
+//  FakeDataFile.swift
+//  VideoGameLibraryApp
+//
+//  Created by Luana Duarte on 30/05/26.
+//
+
+// TODO: - Remover após adicionar impl
---
+++ VideoGameLibraryApp/Domain/FakeDomainFile.swift
@@ -0,0 +1,8 @@
+//
+//  FakeDomainFile.swift
+//  VideoGameLibraryApp
+//
+//  Created by Luana Duarte on 30/05/26.
+//
+
+// TODO: - Remover após adicionar impl
